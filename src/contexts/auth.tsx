c0293d542ced2385727ec0de9668c4b243e40e3d
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2023-03-22 01:27:07 -0300

    feat: config signUp e atualizacao AuthContext, ambos devidamente tipados

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,27 +1,69 @@
 import React, { createContext, useState } from "react";
 
+import { auth, db } from '../services/firebaseConnection'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { doc, getDoc, setDoc } from 'firebase/firestore'
+
 interface AuthProviderProps {
   children: React.ReactNode;
 }
 
 interface AuthContextProps {
   signed: boolean;
-  user: any;
+  user: DataProps | null;
   signIn: (email: string, password: string) => void;
+  signUp: (name: string, email: string, password: string) => void;
+  loadingAuth: boolean;
+}
+
+interface DataProps {
+  uid: string;
+  name: string;
+  email: string | null;
+  avatarUrl: string | null;
 }
 
 export const AuthContext = createContext<AuthContextProps>({
   signed: false,
   user: null,
-  signIn: () => { }
+  signIn: () => { },
+  signUp: () => { },
+  loadingAuth: false
 })
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState<DataProps | null>(null)
+  const [loadingAuth, setLoadingAuth] = useState(false)
 
   function signIn(email: string, password: string) {
-    console.log(email);
-    console.log(password);
+  }
+
+  async function signUp(name: string, email: string, password: string) {
+    setLoadingAuth(true)
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then(async (value) => {
+        let uid = value.user.uid
+
+        await setDoc(doc(db, 'users', uid), {
+          name: name,
+          avatarUrl: null
+        })
+          .then(() => {
+            let data: DataProps = {
+              uid: uid,
+              name: name,
+              email: value.user.email,
+              avatarUrl: null
+            }
+            setUser(data)
+            setLoadingAuth(false)
+          })
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoadingAuth(false)
+      })
   }
 
   return (
@@ -29,7 +71,9 @@ function AuthProvider({ children }: AuthProviderProps) {
       value={{
         signed: !!user,
         user,
-        signIn
+        signIn,
+        signUp,
+        loadingAuth
       }}
     >
       {children}
