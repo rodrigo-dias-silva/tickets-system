f8f1eab4e27ab54a6804efcf58bddc068b2fc2f1
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2023-03-23 12:05:14 -0300

    adiçao lib react-toastify e config no arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
-import RoutesApp from './routes'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 import AuthProvider from './contexts/auth'
+
+import RoutesApp from './routes'
 
 function App() {
 
   return (
     <BrowserRouter>
       <AuthProvider>
+        <ToastContainer autoClose={3000} />
         <RoutesApp />
       </AuthProvider>
     </BrowserRouter>
