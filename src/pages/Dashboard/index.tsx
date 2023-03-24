990d83ae5409e6e50d09c3319193dac9b24043eb
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2023-03-23 22:09:42 -0300

    feat: preludio de botao para logout/signout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/auth'
+import { auth } from '../../services/firebaseConnection';
 
 type Props = {}
 
 export default function Dashboard({ }: Props) {
+  const { logOut } = useContext(AuthContext)
+
+  async function handleLogout() {
+    await logOut(auth);
+  }
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h1>Pagina Dashboard</h1>
+      <button onClick={handleLogout}>Sair da conta</button>
+    </div>
   )
 }
