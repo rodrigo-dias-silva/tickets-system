032eb410e3ddd6c1eb73d93dd9696ec4ce7b2f79
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2023-03-23 22:14:44 -0300

    feat: adiçao do PrivateRoutes para as rotas que precisam de autenticaçao para liberar o acesso

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,22 @@ import { Route, Routes } from 'react-router-dom'
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
 import Dashboard from '../pages/Dashboard'
+import PrivateRoute from './PrivateRoute'
 
 function RoutesApp() {
   return (
     <Routes>
       <Route path='/' element={<SignIn />} />
       <Route path='/register' element={<SignUp />} />
-      <Route path='/dashboard' element={<Dashboard />} />
+
+      <Route
+        path='/dashboard'
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   )
 }
