c8c83e6861a735333ed25935fa325281707ee3ab
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2023-03-25 02:56:09 -0300

    feat: config das funcionalidades do html (input file) e demais funcionalidades de estado. Faltando apenas a config de salvar no firebase

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,8 @@
-import { useContext, useState } from 'react'
+import { ChangeEvent, useContext, useState } from 'react'
 import { Gear, UploadSimple } from '@phosphor-icons/react'
 
 import { AuthContext } from '../../contexts/auth'
+import { auth } from '../../services/firebaseConnection'
 
 import avatar from '../../assets/avatar.png'
 
@@ -12,32 +13,66 @@ type Props = {}
 
 export default function Profile({ }: Props) {
 
-  const { user } = useContext(AuthContext)
+  const { user, setUser, storageUser, logOut } = useContext(AuthContext)
 
   const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
+  const [imageAvatar, setImageAvatar] = useState<File | null>(null)
+  const [name, setName] = useState(user && user.name)
+  const [email, setEmail] = useState(user && user.email)
+
+  function handleFile(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files) {
+      const image = e.target.files[0]
+
+      if (['image/jpeg', 'image/png', 'image/jpg', 'image/gif'].includes(image.type)) {
+        setImageAvatar(image)
+        setAvatarUrl(URL.createObjectURL(image))
+      } else {
+        alert('Envie uma imagem dos tipos PNG, JPG, JPEG ou GIF')
+        setImageAvatar(null)
+        return
+      }
+    }
+  }
 
   return (
     <div>
+
       <Header />
+
       <div className='md:ml-52 px-4 py-1'>
+
         <Title name='Configurações'>
           <Gear size={24} />
         </Title>
 
         <div className='flex bg-light-color rounded-md p-4 items-center mb-4 shadow-md'>
           <form className='w-full'>
-            <div className='group w-72 h-72 flex justify-center items-center flex-col cursor-pointer'>
+            <label className='group w-72 h-72 flex justify-center items-center flex-col cursor-pointer'>
               <span className='z-10 absolute opacity-60 transition-all group-hover:opacity-100 group-hover:scale-125 group-hover:drop-shadow-md'>
-                <UploadSimple size={30} color='#fff' />
+                <UploadSimple
+                  size={30}
+                  color='#fff'
+                />
               </span>
-              <input type="file" accept='image/*' className='hidden' />
-              <img src={avatarUrl ? avatarUrl : avatar} alt='foto de perfil' className='w-64 h-64 rounded-full object-cover mb-4' />
-            </div>
+              <input
+                type="file"
+                accept='image/*'
+                className='hidden'
+                onChange={handleFile}
+              />
+              <img
+                src={avatarUrl ? avatarUrl : avatar}
+                alt='foto de perfil'
+                className='w-64 h-64 rounded-full object-cover mb-4'
+              />
+            </label>
             <div className='flex flex-col gap-3 w-full'>
               <span className=''>Nome</span>
               <input
                 type="text"
-                placeholder='Seu nome'
+                value={name ? name : ''}
+                onChange={(e) => setName(e.target.value)}
                 className='w-full h-10 rounded p-3 text-sm max-w-xl outline-none mb-4'
               />
             </div>
@@ -46,7 +81,7 @@ export default function Profile({ }: Props) {
               <span className=''>E-mail</span>
               <input
                 type="text"
-                placeholder='teste@teste.com'
+                value={email ? email : ''}
                 disabled
                 className='cursor-not-allowed w-full max-w-xl h-10 rounded p-3 text-sm mb-4'
               />
@@ -62,7 +97,12 @@ export default function Profile({ }: Props) {
         </div>
 
         <div className='flex bg-light-color rounded-md p-4 items-center mb-4 shadow-md'>
-          <button className='w-36 h-10 rounded p-3 text-lg text-dark-blue bg-transparent border border-dark-blue flex items-center justify-center font-semibold hover:bg-dark-blue hover:text-light-color transition-all shadow-md hover:shadow-none'>Sair</button>
+          <button
+            className='w-36 h-10 rounded p-3 text-lg text-dark-blue bg-transparent border border-dark-blue flex items-center justify-center font-semibold hover:bg-dark-blue hover:text-light-color transition-all shadow-md hover:shadow-none'
+            onClick={() => logOut(auth)}
+          >
+            Sair
+          </button>
         </div>
       </div>
     </div>
